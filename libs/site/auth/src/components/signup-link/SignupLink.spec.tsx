6d1651bc70f1b6b2668dd623b3287a9975df4b7b
Guga Zimmermann <gugazimmermann@gmail.com>
2021-09-01 19:17:06 -0300

    forgot password

--- libs/site/auth/src/components/signup-link/SignupLink.spec.tsx
+++ libs/site/auth/src/components/signup-link/SignupLink.spec.tsx
@@ -1,10 +1,10 @@
 import { render } from "@testing-library/react";
-
+import { MemoryRouter } from 'react-router';
 import SignupLink from "./SignupLink";
 
 describe("SignupLink", () => {
   it("should render successfully", () => {
-    const { baseElement } = render(<SignupLink />);
+    const { baseElement } = render(<MemoryRouter><SignupLink /></MemoryRouter>);
     expect(baseElement).toBeTruthy();
   });
 });
