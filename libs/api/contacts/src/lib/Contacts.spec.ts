8ca1449cc1edd0c5e10c60d516a0a3be4623de77
Guga Zimmermann <gugazimmermann@gmail.com>
2021-09-16 16:40:43 -0300

    contacts api

--- libs/api/contacts/src/lib/Contacts.spec.ts
+++ libs/api/contacts/src/lib/Contacts.spec.ts
@@ -1,7 +1,18 @@
-import { Contacts } from "./Contacts";
+import * as request from "supertest";
+import * as express from "express";
+import Contacts from './Contacts'
 
-describe("Contacts", () => {
-  it("should work", () => {
-    expect(Contacts()).toEqual("api-contacts");
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+Contacts(app);
+
+describe("contacts", () => {
+
+  test("/api/public should response the GET method", async () => {
+    const response = await request(app).get("/api/public");
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toBe('Public Content.')
   });
 });
+
