9490395c2e23397f2b5876a6eb7d50ef17f4bede
Guga Zimmermann <gugazimmermann@gmail.com>
2021-09-16 23:07:49 -0300

    contacts site

--- libs/site/contacts/src/lib/details/Details.spec.tsx
+++ libs/site/contacts/src/lib/details/Details.spec.tsx
@@ -1,10 +1,17 @@
 import { render } from "@testing-library/react";
-
+import { MemoryRouter } from "react-router-dom";
 import Details from "./Details";
 
 describe("Details", () => {
   it("should render successfully", () => {
-    const { baseElement } = render(<Details />);
+    const data = {
+      name: "AAA",
+    };
+    const { baseElement } = render(
+      <MemoryRouter>
+        <Details data={data} />
+      </MemoryRouter>
+    );
     expect(baseElement).toBeTruthy();
   });
 });
