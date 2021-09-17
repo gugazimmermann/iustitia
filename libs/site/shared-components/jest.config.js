a37a8dbac383a6f98dc39acc95fdfa1b86533896
Guga Zimmermann <gugazimmermann@gmail.com>
2021-09-17 03:50:49 -0300

    attachment modal

--- libs/site/shared-components/jest.config.js
+++ libs/site/shared-components/jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   preset: "../../../jest.preset.js",
   transform: {
     "^.+\\.[tj]sx?$": "babel-jest",
+    ".+\\.(css|scss|png|jpg|svg)$": "jest-transform-stub",
   },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
   coverageDirectory: "../../../coverage/libs/site/shared-components",
