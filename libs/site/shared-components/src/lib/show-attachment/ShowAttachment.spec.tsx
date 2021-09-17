75382297ead86f84f59d753562217bb5eac96e15
Guga Zimmermann <gugazimmermann@gmail.com>
2021-09-16 22:54:42 -0300

    contacts site

--- libs/site/shared-components/src/lib/show-attachment/ShowAttachment.spec.tsx
+++ libs/site/shared-components/src/lib/show-attachment/ShowAttachment.spec.tsx
@@ -1,10 +1,19 @@
 import { render } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import ShowAttachment from "./ShowAttachment";
 
-import Alert from "./ShowAttachment";
-
-describe("Alert", () => {
+describe("ShowAttachment", () => {
   it("should render successfully", () => {
-    const { baseElement } = render(<Alert />);
+    const att = {
+      date: "01/01/2021 00:00",
+      name: "AAA",
+      link: "http://teste.com",
+    };
+    const { baseElement } = render(
+      <MemoryRouter>
+        <ShowAttachment attachment={att} />
+      </MemoryRouter>
+    );
     expect(baseElement).toBeTruthy();
   });
 });
